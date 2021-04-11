6aec10cdc194d8bb189431c69fd573ba68107221
SuperOrca <69650371+SuperOrca@users.noreply.github.com>
2021-04-11 11:15:31 -0400

    test

--- src/cogs/network.py
+++ src/cogs/network.py
@@ -6,23 +6,25 @@ from discord.ext import commands
 
 from .api.color import color
 
-
 class Network(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.command(name='network')
     async def _network(self, ctx):
-        async with ctx.typing():
-            embed = Embed(title='Network', timestamp=datetime.now(), color=color.minehut).set_footer(
-                text="Requested by " + ctx.author)
-            embed.add_field(name='Players Online', value=f"{minehut.getPlayerCount():,d}")
-            embed.add_field(name='Severs Online', value=f"{minehut.getServerCount():,d}")
-            embed.add_field(name='Server Max', value=f"{minehut.getServerCap():,d}")
-            embed.add_field(name='Top Servers', value="".join(
-                [f"{index}. {server.getName()} ({server.getPlayerCount():,d} players)" for index, server in
-                 enumerate(minehut.getTop5(), start=1)]), inline=False)
-        await ctx.send(embed=embed)
+        try:
+            async with ctx.typing():
+                embed = Embed(title='Network', timestamp=datetime.now(), color=color.minehut).set_footer(
+                    text="Requested by " + ctx.author)
+                embed.add_field(name='Players Online', value=f"{minehut.getPlayerCount():,d}")
+                embed.add_field(name='Severs Online', value=f"{minehut.getServerCount():,d}")
+                embed.add_field(name='Server Max', value=f"{minehut.getServerCap():,d}")
+                embed.add_field(name='Top Servers', value="".join(
+                    [f"{index}. {server.getName()} ({server.getPlayerCount():,d} players)" for index, server in
+                     enumerate(minehut.getTop5(), start=1)]), inline=False)
+                await ctx.send(embed=embed)
+        except Exception as e:
+            print(e)
 
 
 def setup(client):
